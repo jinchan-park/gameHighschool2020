c6b68c2d8cd5dc77dc1823098b702f8f387e73aa
parkjinchan <pjchface@gmail.com>
2020-07-01 19:27:38 +0900

    탈알 피하기 게임 최초 푸쉬

---
+++ Dodge/Assets/Dodge/scripts/playerController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    public float m_speed = 13f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Rigidbody rigidbody = gameObject.GetComponent<Rigidbody>();
+
+        float xAxis = Input.GetAxis("Horizontal");
+        float yAxis = Input.GetAxis("Vertical");
+
+        rigidbody.AddForce(new Vector3(xAxis, 0, yAxis) * m_speed);
+
+        float fireAxis = Input.GetAxis("Fire1");
+
+        if (fireAxis > 0.95f)
+            Die();
+
+         /*if (Input.GetKey(KeyCode.LeftArrow))
+         {
+             //transform.position += Vector3.left * m_speed * Time.deltaTime;
+             rigidbody.AddForce(Vector3.left * m_speed);
+             Debug.Log("좌");
+         }
+         else if (Input.GetKey(KeyCode.RightArrow))
+         {
+             //transform.position += Vector3.right * m_speed * Time.deltaTime;
+             rigidbody.AddForce(Vector3.right * m_speed);
+             Debug.Log("우");
+         }
+         else if (Input.GetKey(KeyCode.UpArrow))
+         {
+             //transform.position += Vector3.forward * m_speed * Time.deltaTime;
+             rigidbody.AddForce(Vector3.forward * m_speed);
+             Debug.Log("위");
+         }
+         else if (Input.GetKey(KeyCode.DownArrow))
+         {
+             //transform.position += Vector3.back * m_speed * Time.deltaTime;
+             rigidbody.AddForce(Vector3.back * m_speed);
+             Debug.Log("아래");
+         }*/
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Die();
+        }
+    }
+
+    public void Die()
+    {
+        Debug.Log("사망");
+        gameObject.SetActive(false);
+    }
+}
---
+++ Dodge/Assets/test.cs
@@ -0,0 +1,21 @@
+﻿using System.Security.Claims;
+using UnityEditor;
+using UnityEngine;
+
+public class Test : EditorWindow
+{
+    [MenuItem("Window/test")]
+    static void init()
+    {
+        Test Window = (Test)EditorWindow.GetWindow(typeof(Test));
+        Window.Show();
+    }
+
+    void OnGUI()
+    {
+        Handles.color = Color.red;
+        Handles.DrawRectangle(0, new Vector3(200, 200, 0), Quaternion.identity, 100);
+        Handles.DrawSolidDisc(new Vector3(200, 200, 0), Vector3.forward, 100)
+;            
+    }
+}
