b1aae39672cafed65d17d175b81ce705a1474ba2
parkjinchan <pjchface@gmail.com>
2020-07-20 21:09:15 +0900

    슈팅게임 1일차

---
+++ ShootingGame/Assets/ShootingGame/Script/FireBall.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditorInternal;
+using UnityEngine;
+
+public class FireBall : MonoBehaviour
+{
+    public float m_speed = 1f;
+
+    public float tlqkf = Time.deltaTime; 
+    void Update()
+    {
+       transform.Translate(Vector2.up * m_speed * Time.deltaTime);
+    }
+}
---
+++ ShootingGame/Assets/ShootingGame/Script/PlayerShip.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class PlayerShip : MonoBehaviour
+{
+    public float m_speed = 0f;
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(Vector2.up * m_speed * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(Vector2.down * m_speed * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector2.right * m_speed * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector2.left * m_speed * Time.deltaTime);
+        }
+
+        //float xAxis = Input.GetAxis("Horizontal");
+        //float yAxis = Input.GetAxis("Vertical");
+
+        //Vector2 inputvaiue = new Vector2(xAxis, yAxis).normalized;
+
+
+            
+    }
+}
