5917c0f1825b150680a43117b9d2f2ba0412992d
parkjinchan <pjchface@gmail.com>
2020-09-17 21:19:46 +0900

    sunny end

--- Sunny Land/Assets/m/Scripts/PlayerController.cs
+++ Sunny Land/Assets/m/Scripts/PlayerController.cs
@@ -1,25 +1,48 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using System.Runtime.InteropServices;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    
-    public Transform m_Sprite;
+    public enum MoveState
+    {
+        Run,
+        Jump,
+        Climb
+    }
 
     private Rigidbody2D m_Rigidbody2D;
     private Animator m_Animator;
+
+    [Header("참조")]
+    public Transform m_Sprite;
+    public Collider2D m_WorkCollider;
+    public Collider2D m_SideCollider;
+
+    [Header("속성")]
     public float m_XAxisSpeed = 3f;
     public float m_YJumpPower = 3f;
-    public bool m_IsTouchLadder = false;
-    public bool m_m_IsClimbing = false;
     public float m_ClimbSpeed = 2f;
 
-    public bool m_IsJumping = false;
-    public bool m_IsClimbing = false;
-
+    [Header("내부 속성(차후 private)")]
+    public MoveState m_MoveState;
     public int m_JumpCount = 0;
+
+    public bool m_IsTouchLadder = false;
+    public bool m_IsClimbing = false;
+    public bool m_IsJumping = false;
+
+    public float m_FlightTime = 0f;
+
+    //추가
+    public float m_HitRecoveringTime = 0;
+
+    private bool m_InputJump = false;
+
+    public void Jump()
+    {
+        m_InputJump = true;
+    }
 
     protected void Start()
     {
@@ -27,112 +50,99 @@ public class PlayerController : MonoBehaviour
         m_Animator = GetComponent<Animator>();
     }
 
+    public VariableJoystick m_Joystick;
+
     protected void Update()
     {
+        //Device Input 받아오기
         float xAxis = Input.GetAxis("Horizontal");
         float yAxis = Input.GetAxis("Vertical");
 
-        if(m_IsTouchLadder  == true && Mathf.Abs(yAxis) > 0.5f)
+        xAxis += m_Joystick.Horizontal;
+        yAxis += m_Joystick.Vertical;
+
+        var inputJump = m_InputJump;
+        m_InputJump = false;
+
+        //+= => -=    <=  =>  >
+        m_HitRecoveringTime -= Time.deltaTime;
+        if (m_HitRecoveringTime > 0)
+        {
+            ClimbingExit();
+
+            //수정
+            m_Animator.SetBool("TakingDamage", true);
+            return;
+        }
+        else
+        {
+            //수정
+            m_Animator.SetBool("TakingDamage", false);
+        }
+
+        if (m_IsTouchLadder == true
+            && Mathf.Abs(yAxis) > 0.5f)
         {
             m_IsClimbing = true;
         }
 
         if (!m_IsClimbing)
         {
-
+            //좌우 이동
             Vector2 velocity = m_Rigidbody2D.velocity;
             velocity.x = xAxis * m_XAxisSpeed;
             m_Rigidbody2D.velocity = velocity;
 
+            //캐릭터 좌우 반전
             if (xAxis > 0)
                 m_Sprite.localScale = new Vector3(1, 1, 1);
             else if (xAxis < 0)
                 m_Sprite.localScale = new Vector3(-1, 1, 1);
 
-
-            if (Input.GetKeyDown(KeyCode.UpArrow)
+            //점프 처리
+           if(Input.GetKeyDown(KeyCode.UpArrow) || inputJump
                 && m_JumpCount <= 0)
             {
-                m_Rigidbody2D.AddForce(Vector3.up
-                    * m_YJumpPower);
-
+                m_Rigidbody2D.AddForce(Vector2.up * m_YJumpPower); ;
                 m_JumpCount++;
             }
-            m_Animator = GetComponent<Animator>();
-            var animator = GetComponent<Animator>();
-            animator.SetFloat("VelocityY", velocity.y);
 
+            //이거 추가
             m_IsJumping = Mathf.Abs(velocity.y) >= 0.1f ? true : false;
 
-            m_Animator.SetBool("Isjumping", m_IsJumping);
+            //스크립트 2줄 작성
+            m_Animator.SetBool("IsJumping", m_IsJumping);
             m_Animator.SetFloat("VelocityX", Mathf.Abs(xAxis));
 
-            m_Animator.SetFloat("Velocity", velocity.y);
-
+            //이거 추가
+            m_Animator.SetFloat("VelocityY", velocity.y);
         }
         else
         {
             //사다리 타는 도중의 이동기능
-            m_Rigidbody2D.constraints  = m_Rigidbody2D.constraints |  RigidbodyConstraints2D.FreezePosition;
+            m_Rigidbody2D.constraints =
+                m_Rigidbody2D.constraints
+                | RigidbodyConstraints2D.FreezePosition;
 
             Vector3 movement = Vector3.zero;
-            movement.y = yAxis * m_ClimbSpeed * Time.deltaTime;
             movement.x = xAxis * m_ClimbSpeed * Time.deltaTime;
+            movement.y = yAxis * m_ClimbSpeed * Time.deltaTime;
 
             transform.position += movement;
-            if(Input.GetKeyDown(KeyCode.Space))
+
+            if (Input.GetKeyDown(KeyCode.UpArrow) || inputJump)
             {
                 ClimbingExit();
             }
+
+            // Update 사다리타기 점프 캔슬하는데 
+            //여기랑
             m_Animator.SetBool("IsClimbing", m_IsClimbing);
-            m_Animator.SetFloat("ClimbingSpeed", 
-                Mathf.Abs(xAxis) +Mathf.Abs(yAxis));
+            m_Animator.SetFloat("ClimbSpeed",
+                Mathf.Abs(xAxis) + Mathf.Abs(yAxis));
         }
     }
-    
 
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        foreach (ContactPoint2D contact in collision.contacts)
-        {
-            if(contact.normal.y > 0.5f)
-            {
-                m_JumpCount = 0;
-                if(contact.rigidbody)
-                {
-                    var hp = contact.rigidbody.GetComponent<HPComponent>();
-                    if(hp)
-                    {
-                        //Destroy(hp.gameObject);
-                        hp.TakeDamage(10);
-                    }
-
-                }
-            }
-            else if(contact.rigidbody && contact.rigidbody.tag == "Enemy")
-            {
-                var hp = GetComponent<HPComponent>();
-                hp.TakeDamage(10);
-
-                m_Animator.SetTrigger("TakeDamage");
-            }
-        }
-    }
-    
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if(collision.tag == "Ladder")
-        {
-            m_IsTouchLadder = true;
-        }
-        else if(collision.tag == "Item")
-        {
-            var item = collision.GetComponent<ItemComponet>();
-            if(item != null)
-                item.BeAte();
-
-        }
-    }
     private void ClimbingExit()
     {
         m_Rigidbody2D.constraints =
@@ -140,11 +150,76 @@ public class PlayerController : MonoBehaviour
             & ~RigidbodyConstraints2D.FreezePosition;
         m_IsClimbing = false;
 
+        //여기
         m_Animator.SetBool("IsClimbing", m_IsClimbing);
     }
+
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        foreach (ContactPoint2D contact in collision.contacts)
+        {
+            Debug.DrawRay(contact.point, contact.normal, Color.white);
+            if (contact.normal.y > 0.5f)
+            {
+                m_JumpCount = 0;
+            }
+        }
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        foreach (ContactPoint2D contact in collision.contacts)
+        {
+            if (contact.normal.y > 0.5f)
+            {
+                m_JumpCount = 0;
+
+                if (contact.rigidbody)
+                {
+                    var hp = contact.rigidbody.GetComponent<HPComponent>();
+                    if (hp)
+                    {
+                        //Destroy(hp.gameObject);
+                        hp.TakeDamage(10);
+                    }
+                }
+            }
+            else if (contact.rigidbody && contact.rigidbody.tag == "Enemy")
+            {
+                //피격시 무적
+                if (m_HitRecoveringTime <= 0)
+                {
+                    var hp = GetComponent<HPComponent>();
+                    hp.TakeDamage(10);
+
+                    m_HitRecoveringTime = 1f;
+
+                    //m_Animator.SetTrigger("TakeDamage");
+                }
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Ladder")
+        {
+            m_IsTouchLadder = true;
+        }
+        else if (collision.tag == "Item")
+        {
+            ItemComponet item = collision.
+                GetComponent<ItemComponet>();
+            if (item != null)
+                item.BeAte();
+        }
+    }
+
     private void OnTriggerExit2D(Collider2D collision)
     {
-        if(collision.tag == "Ladder")
+        if (collision.tag == "Ladder")
         {
             m_IsTouchLadder = false;
 
@@ -152,6 +227,5 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-
-    
+    //40분까지 먼저한 사람은 조용히 복습
 }
